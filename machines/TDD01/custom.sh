1a1d59fd64ea0140e35234cafc1cf6d4e96a5cec
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-11-01 00:12:37 +0100

    custom path tdd01

--- machines/TDD01/custom.sh
+++ machines/TDD01/custom.sh
@@ -1,11 +1,14 @@
-export JAVA_HOME=/mnt/c/Dev/Outils/Java/lnx/jdk1.8.0_101
-export GRADLE_HOME=/mnt/c/Dev/Outils/gradle
+export JAVA_HOME=/mnt/E/Dev/Outils/Java/lnx/jdk1.8.0_92
+export GRADLE_HOME=/mnt/E/Dev/Outils/gradle
+export MAVEN_HOME=/mnt/E/Dev/Outils/apache-maven-3.3.9
 export VS_CODE=/mnt/c/'Program Files (x86)'/'Microsoft VS Code'/
+
 
 export PATH="\
 ${VS_CODE}:\
 ${JAVA_HOME}/bin:\
 ${GRADLE_HOME}/bin:\
+${MAVEN_HOME}/bin:\
 /usr/local/bin:\
 /usr/bin:\
 /bin:\
