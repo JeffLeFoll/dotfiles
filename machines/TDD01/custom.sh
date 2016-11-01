8d1a6ae46454eb6703127f9b65263d346a85d753
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-11-01 10:00:00 +0100

    update VS Code path on TDD01

--- machines/TDD01/custom.sh
+++ machines/TDD01/custom.sh
@@ -1,7 +1,7 @@
 export JAVA_HOME=/mnt/E/Dev/Outils/Java/lnx/jdk1.8.0_92
 export GRADLE_HOME=/mnt/E/Dev/Outils/gradle
 export MAVEN_HOME=/mnt/E/Dev/Outils/apache-maven-3.3.9
-export VS_CODE=/mnt/c/'Program Files (x86)'/'Microsoft VS Code'/
+export VS_CODE=/mnt/F/Dev/'Microsoft VS Code'/
 
 
 export PATH="\
